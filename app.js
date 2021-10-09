c137772edc28cba6af73d340ce98769bcd129b93
Kareem00101 <89580954+Kareem00101@users.noreply.github.com>
2021-10-09 22:12:29 +0200

    just some updates
    tested event listeners

---
+++ app.js
@@ -0,0 +1,9 @@
+// some event listener trial
+document.addEventListener('click', function(){
+    console.log("Aww that hurts! xd");
+});
+// yo, lets do something more cool
+document.addEventListener('click', function(){
+    const mainHeading = document.querySelector('h1');
+    mainHeading.style.backgroundColor = 'red';
+});
